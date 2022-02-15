021d0de6df75a450744c43ca950309dd7307a52d
vitorchx@outlook.com <vitorchx@outlook.com>
2022-02-15 05:57:11 -0300

    xx

--- ExercicioVariaveisEtiposPrimitivos/variaveisEtiposPrimitivos.java
+++ ExercicioVariaveisEtiposPrimitivos/variaveisEtiposPrimitivos.java
@@ -1,3 +1,4 @@
+package ExercicioVariaveisEtiposPrimitivos;
 /* Na	 empresa	 em	 que	trabalhamos,	 há	tabelas	 com	 o	 gasto	 de	 cada	 mês.	 Para	 fechar	 o	 balanço	 do
 primeiro	trimestre,	 precisamos	 somar	 o	 gasto	total.	Sabendo	 que,	em	janeiro,	 foram	 gastos	 15	mil
 reais,	em	fevereiro,	23	mil	reais	e,	em	março,	17	mil	reais,	faça	um	programa	que	calcule	e	imprima
